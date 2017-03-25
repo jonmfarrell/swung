bfa7b078f88987b890125fa43a201007402e0cf7
Nathan <Burgess@Nathans-MacBook-Pro.local>
2017-03-25 15:50:28 -0400

    eh

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  # root "welcome/#index"
   resources :users, :only => [:index, :show]
   resources :teams, :only => [:show]
   resources :outings, :only => [:create]
