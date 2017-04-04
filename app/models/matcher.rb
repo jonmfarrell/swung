690c1134849a397bbf9d81e7b7da8fe3566fa628
Jon Farrell <jon@jonfarrell.io>
2017-04-03 21:40:55 -0400

    changing database.yml

--- app/models/matcher.rb
+++ app/models/matcher.rb
@@ -2,7 +2,7 @@
 
 require "dbi"
 
-class matchMe < ActiveRecord::Base
+class MatchMe
 
 	# the users being checked for matchability
 	# TODO: actually fetch them right because this is probably not gonna work
